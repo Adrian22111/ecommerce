19c39524811cd1fb735b32692d7574e39e757dc0
root <root@DESKTOP-3AF2ODS.localdomain>
2025-06-29 20:20:49 +0200

    basic routing

---
+++ app/src/Controller/Admin/AdminPanelController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class AdminPanelController extends AbstractController
+{
+    #[Route('/', name: 'panel')]
+    public function index(): Response
+    {
+        return $this->render('admin/panel.twig.', [
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/AdminPanelController.php',
+        ]);
+    }
+}
