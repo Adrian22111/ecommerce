ce163bc152f8ad7b92270980a52824eccfdb0df7
adrian22111 <adrianszymczyk2211@gmail.com>
2025-09-09 20:27:48 +0200

    vich uninstallation

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -11,7 +11,6 @@ return [
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
-    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
--- app/src/Entity/ProductImage.php
+++ app/src/Entity/ProductImage.php
@@ -4,11 +4,8 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\ProductImageRepository;
-use Symfony\Component\HttpFoundation\File\File;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: ProductImageRepository::class)]
-#[Vich\Uploadable]
 class ProductImage
 {
     #[ORM\Id]
@@ -18,24 +15,6 @@ class ProductImage
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $name = null;
-
-    #[Vich\UploadableField(
-        mapping: 'product',
-        fileNameProperty: 'name',
-        size: 'size',
-        mimeType: 'mimeType',
-        dimensions: 'dimensions'
-    )]
-    private ?File $file = null;
-
-    #[ORM\Column(nullable: true)]
-    private ?int $size = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $mimeType = null;
-
-    #[ORM\Column(type: 'json', nullable: true)]
-    private ?array $dimensions = null;
 
     #[ORM\ManyToOne(inversedBy: 'productImages')]
     #[ORM\JoinColumn(nullable: false)]
@@ -61,46 +40,6 @@ class ProductImage
         return $this;
     }
 
-    public function getFile(): ?File
-    {
-        return $this->file;
-    }
-
-    public function setFile(?File $file = null): static
-    {
-        $this->file = $file;
-
-        if (null !== $file) {
-            $this->lastUpdate = new \DateTimeImmutable();
-        }
-
-        return $this;
-    }
-
-    public function getSize(): ?int
-    {
-        return $this->size;
-    }
-
-    public function setSize(?int $size = null): static
-    {
-        $this->size = $size;
-
-        return $this;
-    }
-
-    public function getMimeType(): ?string
-    {
-        return $this->mimeType;
-    }
-
-    public function setMimeType(?string $mimeType = null): static
-    {
-        $this->mimeType = $mimeType;
-
-        return $this;
-    }
-
     public function getLastUpdate(): ?\DateTimeImmutable
     {
         return $this->lastUpdate;
@@ -109,18 +48,6 @@ class ProductImage
     public function setLastUpdate(\DateTimeImmutable $lastUpdate): static
     {
         $this->lastUpdate = $lastUpdate;
-
-        return $this;
-    }
-
-    public function getDimensions(): ?array
-    {
-        return $this->dimensions;
-    }
-
-    public function setDimensions(?array $dimensions): static
-    {
-        $this->dimensions = $dimensions;
 
         return $this;
     }
